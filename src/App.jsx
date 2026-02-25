17cd22aea936fdda3401f8088ee87ad91b39890e
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-02-25 18:24:26 +0530

    AddTocart

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { CART_STORAGE_KEY, getitem, setitem } from '../../React Project Repo/React-By-Sheryians/src/utils/storage';
+import { CART_STORAGE_KEY, getitem, setitem } from './utils/storage';
+import useCart from './hooks/useCart';
 
 const App = () => {
   setitem(CART_STORAGE_KEY, [{ id: 1, name: "Test", price: 100, qty: 2 }]);
   console.log(getitem(CART_STORAGE_KEY));
-  return (
-    <div>App
+useCart()
 
+  return (
+    <div>
 
     </div>
   )
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
+import { CART_STORAGE_KEY, getitem, setitem } from '../utils/storage'
+import { useState } from 'react';
 
 const useCart = () => {
-    const remmoveitem= (productId)=>{
+  const [caritem, setcaritem] = useState(getitem(CART_STORAGE_KEY))
+  console.log(caritem);
 
-    }
+  const addToCart = (product) => {
+    setcaritem(previtem => {
+      let existiing = previtem.find(item => item.id == product.id)
+      if (existiing) {
+        return previtem.map(item => {
+          item.id === product.id ? { ...item, qty: qty + 1 } : item
+        })
+      }
+      else {
+        return [...previtem, { ...product, qty: 1 }]
+      }
+    })
+  }
+  const removeFromCart = (productId) => {
+    setitem(previtem => {
+      let updateCart = previtem.filter(
+        item => item.id !== productId
+      )
+    })
+  }
   return (
 
 
 
-<div>
+    <div>
 
     </div>
   )
